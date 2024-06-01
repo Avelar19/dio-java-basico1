eefbf5088c8d744247c23125a8ef1d69292a060b
thi_a <thiago.avelar19@gmail.com>
2024-06-01 11:51:28 -0300

    Commit Teste

---
+++ src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá turma");
+        System.out.println("Olá turma1");
+        System.out.println("Olá turma2");
+    }
+}
